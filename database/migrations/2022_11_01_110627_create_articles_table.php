f0d9f4f2f55aa6cc740d5365a3b7ef93b437d712
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2022-11-01 18:09:39 +0700

    add Article model & migration file

---
+++ database/migrations/2022_11_01_110627_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->text('content');
+            $table->string('article_image', 200)->nullable();
+            $table->string('article_creator', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
